8b4b3a6a9b87bc1868ea0591fcf088c4c909c16e
Jaehi <damnyomoonwon1003@gmail.com>
2022-03-25 15:32:26 +0900

    NewMemoFragment / insert()

--- app/src/main/java/com/applemango/memodemo/NewMemoFragment.kt
+++ app/src/main/java/com/applemango/memodemo/NewMemoFragment.kt
@@ -1,71 +1,40 @@
 package com.applemango.memodemo
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.applemango.memodemo.databinding.FragmentNewMemoBinding
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import java.util.EnumSet.of
-import java.util.Optional.of
-import java.util.Set.of
 
-
-@SuppressLint("UseRequireInsteadOfGet")
 class NewMemoFragment : Fragment() {
 
-    private val bind : FragmentNewMemoBinding by lazy {
+    private val bind: FragmentNewMemoBinding by lazy {
         FragmentNewMemoBinding.inflate(LayoutInflater.from(context))
-
     }
 
-//    private val viewmodel : MainViewModel by lazy {
-//        ViewModelProvider(this,MainViewModel.Factory(activity!!.application)).get(MainViewModel::class.java)
-//    }
-    private lateinit var viewmodel : MainViewModel
+    private lateinit var viewModel: NewViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
-    ): View? {
-        viewmodel = requireActivity().run {
-            ViewModelProvider(this).get(MainViewModel::class.java)
+    ): View {
+        viewModel = requireActivity().run {
+            ViewModelProvider(this).get(NewViewModel::class.java)
         }
-
         bind.lifecycleOwner = this
-        bind.viewmodel = viewmodel
-
-
-        viewmodel.getData().observe(viewLifecycleOwner, object : Observer<List<MemoData>>{
-
-            override fun onChanged(t: List<MemoData>?) {
-
-                Log.d("9999999999999","$t")
-            }
-
-        })
-
-        bind.btSave.setOnClickListener(object : View.OnClickListener{
-            override fun onClick(p0: View?) {
-                lifecycleScope.launch(Dispatchers.IO) {
-                    viewmodel.insert(bind.etTitle.text.toString(),bind.etContents.text.toString())
-                }
-            }
-
-        })
-
-
-
-
+        bind.viewmodel = viewModel
+        initUi()
         return bind.root
     }
 
+    private fun initUi() {
+        with(bind) {
+            btSave.setOnClickListener {
+                viewModel.insert()
+            }
+        }
+    }
 }
---
+++ app/src/main/java/com/applemango/memodemo/NewViewModel.kt
@@ -0,0 +1,42 @@
+package com.applemango.memodemo
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import androidx.room.Room
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class NewViewModel(application: Application) : AndroidViewModel(application) {
+
+    val db = Room.databaseBuilder(application, MemoDataBase::class.java, "db-memo").build()
+    val activityEvents = MutableLiveData<ActivityEvents?>(null)
+    var title = ""
+    var contents = ""
+
+    fun insert() {
+        viewModelScope.launch(Dispatchers.IO) {
+            when {
+                title.isEmpty() -> event(ActivityEvents.Something("제목이 비엇숨"))
+                contents.isEmpty() -> event(ActivityEvents.Something("내용이 비엇숨"))
+            }
+            if (title.isNotEmpty() && contents.isNotEmpty()) {
+                db.MemoDao().insert(MemoData(title, contents))
+                title = ""
+                contents = ""
+            }
+        }
+    }
+
+    private fun event(event: ActivityEvents) {
+        viewModelScope.launch(Dispatchers.Main) {
+            activityEvents.value = event
+        }
+    }
+
+    sealed class ActivityEvents {
+        object InsertDataIsEmpty : ActivityEvents()
+        data class Something(val msg: String) : ActivityEvents()
+    }
+}
