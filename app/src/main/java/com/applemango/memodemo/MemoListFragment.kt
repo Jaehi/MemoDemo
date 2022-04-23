64e09611666e61564d6bb3656e63f937e08d66af
Jaehi <damnyomoonwon1003@gmail.com>
2022-04-23 15:54:23 +0900

    Recycler Item Click Event

--- app/src/main/java/com/applemango/memodemo/MemoListFragment.kt
+++ app/src/main/java/com/applemango/memodemo/MemoListFragment.kt
@@ -2,54 +2,54 @@ package com.applemango.memodemo
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.applemango.memodemo.databinding.FragmentMemoListBinding
 
-class MemoListFragment : Fragment() {
+class MemoListFragment : androidx.fragment.app.Fragment() {
 
-    private val bind : FragmentMemoListBinding by lazy {
+    private val bind: FragmentMemoListBinding by lazy {
         FragmentMemoListBinding.inflate(LayoutInflater.from(context))
     }
-    private val viewModel : ListViewModel by viewModels()
+    private val viewModel: ListViewModel by viewModels()
 
     override fun onCreateView(
 
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
 
     ): View {
 
         bind.mRecyclerView.layoutManager = LinearLayoutManager(context)
 
         viewModel.memoList.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
-            bind.mRecyclerView.adapter = MemoAdapter(it) { title, content ,id->
+            bind.mRecyclerView.adapter = MemoAdapter(it, onClickDelete = { title, content, id ->
                 Log.d("delete", "onCreateView: $title , $content $id")
                 viewModel.delete(title, content, id)
-            }
+            },
+                    onClick = { position ->
+                        val bundle = bundleOf("position" to position)
+                        val resultFragment = ResultFragment()
+                        resultFragment.arguments = bundle
+                        findNavController().navigate(R.id.action_memoListFragment_to_resultFragment)
+                    }
+            )
         })
-
 
         initUI()
         return bind.root
     }
 
-    private fun initUI(){
+    private fun initUI() {
         bind.btnBack.setOnClickListener {
             findNavController().popBackStack()
         }
     }
-
-    private fun onClick(){
-
-    }
-
 
 
 }
